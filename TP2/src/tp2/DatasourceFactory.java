238dd31262020611768be1d4fed5ea27569da772
nidduzzi <74892632+nidduzzi@users.noreply.github.com>
2021-03-15 16:43:30 +0700

    layouts, multi panels, images, jdbc mysql

---
+++ TP2/src/tp2/DatasourceFactory.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+/**
+ *
+ * @author ahmad
+ */
+public class DatasourceFactory {
+
+    public static DataSource getMySQLDataSource() {
+        Properties props = new Properties();
+        FileInputStream fis = null;
+        MysqlDataSource mysqlDS = null;
+        try {
+            fis = new FileInputStream("db.properties");
+            props.load(fis);
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setUser(props.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mysqlDS;
+    }
+
+    private static void testDataSource(String dbType) {
+        DataSource ds = null;
+        if ("mysql".equals(dbType)) {
+            ds = getMySQLDataSource();
+        } else {
+            System.out.println("invalid db type");
+            return;
+        }
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            con = ds.getConnection();
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("select empid, name from Employee");
+            while (rs.next()) {
+                System.out.println("Employee ID=" + rs.getInt("empid") + ", Name=" + rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
